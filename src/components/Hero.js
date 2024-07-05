b0b60d1708867a98d294f4e4de4391eff41ff61f
Markcoy <macoy.velasco12@gmail.com>
2024-07-06 00:58:49 +0800

    updated-first

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profilepic from "../assets/macoy.png";
+import profilepic from "../assets/profile_2.png";
 import pic1 from "../assets/p1.png";
 import pic2 from "../assets/p2.png";
 import pic3 from "../assets/p4.png";
@@ -21,20 +21,20 @@ const Hero = () => {
   return (
     <div className="mt-10">
       <div className="  my-7 sm:my-0 max-w-[1200px] h-auto mx-auto flex flex-col-reverse sm:flex-row justify-center align-center">
-        <div className="flex-col my-auto mx-auto md:mx-0">
+        <div className="flex-col my-auto mx-auto ml-6 md:mx-0">
           <p className="md:text-5xl sm:text-4xl text-xl font-bold text-gray-300">
             Hi! I am Macoy Velasco
           </p>
           <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
             <TypeAnimation
               sequence={[
-                "Frontend Dev",
+                "Front-end Dev",
                 1000,
                 "UI/UX Designer",
                 1000,
-                "Web Designer",
-                1000,
                 "Network Dev",
+                1000,
+                "IT Support",
                 1000,
               ]}
               wrapper="span"
@@ -48,7 +48,7 @@ const Hero = () => {
               DLSU - D Fresh Graduate
             </p>
           </div>
-          <div className="text-4xl md:text-5xl flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-14 my-7 text-purple-600">
+          <div className="text-4xl md:text-5xl  flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-14 my-7 text-purple-600">
             <a
               href="https://www.linkedin.com/in/mark-christian-velasco-3138a0250/"
               title="Visit my LinkedIn profile"
@@ -107,43 +107,37 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className="my-auto ">
+        <div className="my-auto  ">
           <img
-            className="lg:w-[400px] md:w-[200px] sm:w-[100px] ml-10 mx-auto h-auto"
+            className="lg:w-[400px] md:w-[200px] sm:w-[100px] ml-auto mx-auto  h-auto"
             src={profilepic}
             alt="Profile pic"
           />
         </div>
       </div>
       {/* For Pics */}
-      <div className="flex justify-center mt-2 ">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-1 sm:gap-1 lg:gap-8 ">
+      <div className="flex justify-center mt-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 sm:gap-1 lg:gap-8">
           {/* Image 1 */}
-
-          <div
-            className="group h-48 w-48 md:h-64 md:w-64 overflow-hidden rounded-lg shadow-lg 
-                 "
-          >
+          <div className="group h-48 w-48 sm:h-64 sm:w-64 md:h-64 md:w-64 overflow-hidden rounded-lg shadow-lg">
             <ImageViewer src={pic1} alt="Pic 1 image" />
           </div>
 
           {/* Image 2 */}
-
-          <div className="group h-48 w-48 md:h-64 md:w-64 overflow-hidden rounded-lg shadow-lg ">
+          <div className="group h-48 w-48 sm:h-64 sm:w-64 md:h-64 md:w-64 overflow-hidden rounded-lg shadow-lg">
             <ImageViewer src={pic2} alt="Pic 2 image" />
           </div>
 
           {/* Image 3 */}
-          <div className="group h-48 w-48 md:h-64 md:w-64 overflow-hidden rounded-lg shadow-lg ">
+          <div className="group h-48 w-48 sm:h-64 sm:w-64 md:h-64 md:w-64 overflow-hidden rounded-lg shadow-lg">
             <ImageViewer src={pic3} alt="Pic 3 image" />
           </div>
 
-          {/* Image 3 */}
-          <div className="group h-48 w-48 md:h-64 md:w-64 overflow-hidden rounded-lg shadow-lg ">
-            <ImageViewer src={pic4} alt="Pic 3 image" />
+          {/* Image 4 */}
+          <div className="group h-48 w-48 sm:h-64 sm:w-64 md:h-64 md:w-64 overflow-hidden rounded-lg shadow-lg">
+            <ImageViewer src={pic4} alt="Pic 4 image" />
           </div>
         </div>
-        {/* End of Pics*/}
       </div>
 
       <About />
